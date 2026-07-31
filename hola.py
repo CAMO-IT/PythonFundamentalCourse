c77795a3af2f5323e036b39f731b816361eed734
CAMO-IT <mc_aleao@hotmail.com>
2026-07-31 00:06:05 -0400

    segunda sesion PE

---
+++ hola.py
@@ -0,0 +1,24 @@
+'''
+    primera sesion del curso
+    python essential
+    por CAMO ACADEMY
+'''
+# primer script Hola mundo
+print("Hola mundo!!")
+
+# uso de sep y end
+print("amo","las","clases","de","python",sep="**",end="/--/")
+print("seguire estudiando")
+
+# uso de funcion print y input
+nombre = input("Buenas noches, ingresa tu nombre:")
+print("hola "+nombre)
+
+
+
+# imprima 5 veces hola 
+
+c=0
+while c<=5:
+    print('hola') # ejemplo solo para ver identacion
+    c += 1
---
+++ prueba.py
@@ -0,0 +1,8 @@
+print("Hola ")
+edad = 18
+# si edad >= 20 pase a la fiesta, si no es mayor entonces no puede entrar
+if edad>=20:
+    print("pase a la fiesta")
+    print("cortesia de la casa ")
+else:
+    #print("no puede entrar a la fiesta")
